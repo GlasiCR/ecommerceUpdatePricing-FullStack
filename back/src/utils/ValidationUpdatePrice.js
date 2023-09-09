676939677aa2ae08d3f969daa6fe9d39372ac324
Glasielle <116851717+GlasiCR@users.noreply.github.com>
2023-09-08 23:06:57 -0300

    Update ValidationUpdatePrice.js

--- back/src/utils/ValidationUpdatePrice.js
+++ back/src/utils/ValidationUpdatePrice.js
@@ -2,11 +2,11 @@ import * as yup from "yup";
 
 class ValidationUpdatePrice {
   static async isValid(data) {
-    const validationProductsSchema = yup.object().shape([{
+    const validationProductsSchema = yup.object().shape({
       code: yup.number().required("Campo obrigatório"),
       name: yup.string().required("Campo obrigatório"),
       new_price: yup.number().required("Campo obrigatório"),
-    }]);
+    });
 
     try {
       await validationProductsSchema.validate(data);
